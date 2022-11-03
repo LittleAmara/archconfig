a2eae12480a5208db76e7f7fadd1d17dbc5a9301
Alexandre Guenot <alexandre.guenot@epita.fr>
2022-11-03 11:40:52 +0100

    feat: added notetaker script

---
+++ .local/bin/notetaker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+NOTESPATH="$HOME/notes/note-$(date +%Y-%m-%d).md"
+
+if [ ! -f $NOTESPATH ]; then
+  echo "# Notes for $(date +%Y-%m-%d)" > $NOTESPATH
+fi
+
+nvim -c "norm Go" \
+  -c "norm Go## $(date +%H:%M)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $NOTESPATH
