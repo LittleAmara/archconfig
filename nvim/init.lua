d53d34ff5a005eb52e9efc2fe613abd2287348b6
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-04-10 15:30:37 +0200

    feat(nvim): trying another architecture

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+require('impatient')
+
+-- ##################### --
+-- # Lua configuration # --
+-- ##################### --
+
+require('packer-config')
+require('keymaps')
+require('settings')
+require('catppuccin-config')
+require('lsp-config.language-servers')
+require('lsp-config.nvim-cmp')
+require('treesitter-config')
+require('telescope-config')
+require('feline-config')
+require('gitsigns-config')
+-- require('coc-config')
