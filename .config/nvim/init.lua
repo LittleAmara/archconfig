9d660d82a0fd9c3c5e475b7f78327f4ad6ea956c
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-04-10 15:01:25 +0200

    chore(nvim: init): clean up the init file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ require('impatient')
 require('packer-config')
 require('keymaps')
 require('settings')
+require('catppuccin-config')
 require('lsp-config.language-servers')
 require('lsp-config.nvim-cmp')
 require('treesitter-config')
@@ -14,19 +15,3 @@ require('telescope-config')
 require('feline-config')
 require('gitsigns-config')
 -- require('coc-config')
-
--- ############### --
--- # COLORSCHEME # --
--- ############### --
-vim.cmd.colorscheme "catppuccin-frappe"
--- Uncomment the next line if you want your background
--- colour to be same same as your terminal
--- vim.cmd "highlight Normal guibg=NONE ctermbg=NONE"
-
--- ################################################### --
--- # Now popup windows have a transparent background # --
--- ################################################### --
-
-vim.cmd "highlight Pmenu guibg=None"
-vim.cmd "highlight PmenuSel guibg=None"
-vim.cmd "highlight NormalFloat guibg=None"
