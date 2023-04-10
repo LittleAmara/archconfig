289790dd1c0e549611e1a349115dc8df060b02b6
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-04-10 17:45:33 +0200

    feat(install): add new script to install dotfiles

--- install.sh
+++ install.sh
@@ -3,22 +3,28 @@
 set -e
 
 REPO_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CONFIGS="$(ls -d ${REPO_PATH}/*)"
+CURRENT_CONFIG=
+
 BACKUP_DIR="${HOME}/.dotfiles.backup/"
+mkdir -p "$BACKUP_DIR"
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+RESET='\033[0m'
 
 fatal() {
-    echo -e "${RED}$*"
+    echo -e "${RED}[${CURRENT_CONFIG}]$2"
     exit 1
 }
 
 info() {
-    echo -e "${BLUE}$*"
+    echo -e "${BLUE}[${GREEN}${CURRENT_CONFIG}${BLUE}]: $*${RESET}"
 }
 
 good() {
-    echo -e "${GREEN}$*"
+    echo -e "${GREEN}[${BLUE}${CURRENT_CONFIG}${GREEN}]: $*${RESET}"
 }
 
 make_backup() {
@@ -26,23 +32,33 @@ make_backup() {
 
     local TARGET="$1"
 
-    mkdir -p "$BACKUP_DIR"
-    [[ -f "TARGET" ]] && mv "$TARGET" "$BACKUP_DIR"
-    [[ -d "TARGET" ]] && mv "$TARGET" "$BACKUP_DIR"
-    info "$TARGET is backed up in $BACKUP_DIR"
+    [[ -L "$TARGET" ]] && info "$1 is a already a symlink, skipping it\n" && return 1
+    [ \( -f "$TARGET" \) -o \( -d "$TARGET" \) ] \
+        && mv --backup=numbered "$TARGET" "$BACKUP_DIR" \
+        && info "$TARGET is backed up in $BACKUP_DIR" \
+        || info "$1 doesn't exists, skipping backup" && return 0
 }
 
-# Requires an argument : the subpath to the directory in this repo
 install_config() {
     [[ "$#" -eq 0 ]] && fatal "You need to specify a config directory/file to install"
 
-    local TARGET="${REPO_PATH}/$1"
-    local DST="${HOME}/$1"
+    local FILENAME=$(basename "$1")
+    CURRENT_CONFIG=$FILENAME
 
-    ln -sivb "$TARGET" "$DST"
-    good "$TARGET is installed in $DST"
+    info "$FILENAME" "Installing configuration"
+
+    local TARGET="$1"
+    local DST="${HOME}/.config/$FILENAME"
+
+    make_backup "$DST"
+    [[ "$?" -eq "1" ]] && echo && exit 0
+
+    ln -sivb --no-target-directory "$TARGET" "$DST"
+    good "Configuration is installed in $DST!"
+    echo
 }
 
-# Setup the nvim config
-make_backup ${HOME}/.config/nvim
-install_config .config/nvim
+for CONFIG in $CONFIGS
+do
+    [[ -d "$CONFIG" ]] && install_config "$CONFIG"
+done
