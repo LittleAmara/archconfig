d2bd689dbd1c72ac4c29b9a90afaa47f002da5fb
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-04-11 22:17:28 +0200

    feat(kitty): still playing with tab bar

--- kitty/tab_bar.py
+++ kitty/tab_bar.py
@@ -12,12 +12,6 @@ from kitty.tab_bar import (
 )
 from kitty.utils import color_as_int
 
-timer_id = None
-
-ICON = "  "
-RIGHT_MARGIN = 1
-REFRESH_TIME = 15
-
 opts = get_options()
 # --------------------------------------------------------------\
 # black  |  red     green    blue     magenta  cyan     white   | color
@@ -28,6 +22,34 @@ opts = get_options()
 icon_fg = as_rgb(color_as_int(opts.color9))
 bat_text_color = as_rgb(color_as_int(opts.color15))
 clock_color = as_rgb(color_as_int(opts.color12))
+pink_color = as_rgb(0x8CAAEE)
+
+ICON = "  "
+PLUGGED_ICON = ""
+UNPLUGGED_ICONS = {
+    10: "",
+    20: "",
+    30: "",
+    40: "",
+    50: "",
+    60: "",
+    70: "",
+    80: "",
+    90: "",
+    100: "",
+}
+UNPLUGGED_COLORS = {
+    15: as_rgb(color_as_int(opts.color1)),
+    16: as_rgb(color_as_int(opts.color15)),
+}
+PLUGGED_COLORS = {
+    15: as_rgb(color_as_int(opts.color1)),
+    16: as_rgb(color_as_int(opts.color6)),
+    99: as_rgb(color_as_int(opts.color6)),
+    100: as_rgb(color_as_int(opts.color2)),
+}
+RIGHT_MARGIN = 1
+REFRESH_TIME = 15
 
 
 def _draw_icon(draw_data: DrawData, screen: Screen, index: int) -> int:
@@ -80,16 +102,44 @@ def _draw_left_status(
     return end
 
 
+def _get_battery_cells() -> list:
+    try:
+        status: str = ""
+        percent: int = 0
+
+        with open("/sys/class/power_supply/BAT1/status", "r") as f:
+            status = f.read()
+        with open("/sys/class/power_supply/BAT1/capacity", "r") as f:
+            percent = int(f.read())
+
+        if status == "Charging\n":
+            icon_color = as_rgb(color_as_int(opts.color10))
+            icon = PLUGGED_ICON
+        else:
+            icon_color = pink_color
+            icon = UNPLUGGED_ICONS[
+                min(UNPLUGGED_ICONS.keys(), key=lambda x: abs(x - percent))
+            ]
+        percent_cell = (bat_text_color, str(percent) + "% ")
+        icon_cell = (icon_color, icon + " ")
+        return [
+            icon_cell,
+            percent_cell,
+        ]
+    except FileNotFoundError:
+        return []
+
+
 def _draw_right_status(screen: Screen, is_last: bool) -> int:
     if not is_last:
         return 0
 
     clock = datetime.now().strftime("%H:%M")
 
+    # cells = _get_battery_cells()
     cells = []
-
     cells.append((clock_color, clock))
-    right_status_length = sum(len(item[1]) for item in cells) + RIGHT_MARGIN
+    right_status_length = sum(len(str(item[1])) for item in cells) + RIGHT_MARGIN
 
     screen.cursor.x = screen.columns - right_status_length
 
