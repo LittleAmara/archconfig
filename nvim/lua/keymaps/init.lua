3f7fbf6e7753648b0bc96ead0266baf186b3be41
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-04-28 00:50:28 +0200

    feat(lua: keymaps): add diagnostic mappings

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -10,15 +10,18 @@ end
 -- Map jk to go back to normal mode
 map('i', 'jk', '<Esc>', {silent = true })
 
--- Map double escape to remove search highlighting
-map('n', '<leader><leader>', ':noh<CR>', {silent = true })
-
--- Keybinds for moving faster between splits
-map('n', '<C-l>', '<C-w>l', {silent = true })
-map('n', '<C-h>', '<C-w>h', {silent = true })
-map('n', '<C-j>', '<C-w>j', {silent = true })
-map('n', '<C-k>', '<C-w>k', {silent = true })
+-- Map double leader to remove search highlighting
+map('n', '<leader><leader>', '<cmd>noh<cr>', {silent = true })
 
 -- Pepega braces
 -- map('i', '{<CR>', '<ESC>o{<CR>}<UP><CR>')
 map('i', '{<CR>', '{<CR>}<UP><END><CR>')
+
+map('n', '<leader>l', '<cmd>Lazy<cr>', {silent = true })
+
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
