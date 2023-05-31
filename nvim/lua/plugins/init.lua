0bded484784bc52c029e3fa159153eea67e770a3
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-05-31 21:21:07 +0200

    feat(nvim): add barbar

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,5 +9,16 @@ return {
             {'-', '<cmd>Oil<cr>', desc = "Open parent directory"},
             {'<leader>-', '<cmd>Oil --float<cr>', desc = "Open parent directory in a floating window"},
         },
-    }
+    },
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            auto_hide = true
+        },
+    },
 }
