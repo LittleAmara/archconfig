696747f2b97cc6ee87eca38335cd8cca2bea1c81
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-06-18 03:23:21 +0200

    feat(nvim): add vim-tmux-navigator

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,16 @@
 return {
     {'folke/lazy.nvim'},
     {
+        'christoomey/vim-tmux-navigator',
+        lazy = false,
+        keys = {
+            {'<C-w>h', '<cmd>TmuxNavigateLeft<cr>', desc = "Switch to left pane"},
+            {'<C-w>l', '<cmd>TmuxNavigateRight<cr>', desc = "Switch to right pane"},
+            {'<C-w>k', '<cmd>TmuxNavigateUp<cr>', desc = "Switch to up pane"},
+            {'<C-w>j', '<cmd>TmuxNavigateDown<cr>', desc = "Switch to down pane"},
+        }
+    },
+    {
         'stevearc/oil.nvim',
         opts = {},
         lazy = false,
